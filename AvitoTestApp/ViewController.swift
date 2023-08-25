59461af2da15e0567bda55fe8dfc59c5580b7664
Dmitry <d1mon227@yandex.ru>
2023-08-25 16:35:24 +0300

    подготовка проекта

--- AvitoTestApp/ViewController.swift
+++ AvitoTestApp/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  AvitoTestApp
-//
-//  Created by Dmitry Medvedev on 25.08.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
